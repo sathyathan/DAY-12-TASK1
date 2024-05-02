1ad0a7c0fa2301396702639c83013ef70e6819bd
sathaythan <sathyathangavel7@gmail.com>
2024-05-02 16:11:45 +0530

    completed

---
+++ script.js
@@ -0,0 +1,15 @@
+//random cat images
+const image =document.getElementById("image")
+const btn =document.getElementById("button")
+const api="https://api.thecatapi.com/v1/images/search"
+const res=fetch(api)
+res.then((data)=>data.json())
+.then((result)=>
+{
+    image.src=result[0].url
+})
+.catch((error)=>error.alert("no images display please refer api"))
+
+btn.addEventListener("click",()=>{
+    window.location.reload();
+})
